a3d868b8d0225341054973346220a5024e1a9196
Shourya Bansal <shouryab2001@gmail.com>
2021-08-26 19:18:15 +0530

    third commit : create constructor and add action images

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,43 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
+        const {price, title, qty}= this.state;
         return(
             <div className="cart-item">
                 <div className="lef-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize: 25} }>Phone</div>
-                    <div style={ {color: '#777'} }>Rs 999</div>
-                    <div style={ {color: '#777'} }>Qty: 1</div>
+                    <div style={ {fontSize: 25} }>{title}</div>
+                    <div style={ {color: '#777'} }>Rs {price}</div>
+                    <div style={ {color: '#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*button*/}
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/992/992651.png" 
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/992/992683.png" 
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://img-premium.flaticon.com/png/512/484/premium/484611.png?token=exp=1629985995~hmac=b4aa1deb651b98762bce549e42fee052"
+                        />
                     </div>
                 </div>
             </div>
